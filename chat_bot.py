abaccea891aa035b65c4c21e9a321cc05027a5c0
Destiny <omoruyidestiny30@gmail.com>
2023-04-29 10:25:50 +0100

    wrote bot functions with chatGPT api

---
+++ chat_bot.py
@@ -0,0 +1,36 @@
+import openai
+from dotenv import dotenv_values
+
+
+openai.api_key = dotenv_values(".env")["API_KEY"]
+
+msg_logs = []
+
+def bot(msg):
+
+    while True:
+
+        if msg == '':
+           result = 'did not get that!'
+        else:
+            msg_logs.append({"role":"user", "content":msg})
+            try:
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=msg_logs
+                )
+
+                result = response["choices"][0]["message"]["content"].strip("\n").strip()
+                # .strip("\n").strip()
+
+            except Exception:
+                result = "Please kindly check your connection"
+            
+        return result
+    
+
+
+
+
+
+    
